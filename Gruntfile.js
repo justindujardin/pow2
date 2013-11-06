8251d32f8f55d3e98b6b1652d7c9a1b9463db7f0
Justin DuJardin <justin@dujardinconsulting.com>
2013-11-06 15:18:23 -0800

    Uglify is okay, it was some weird issue with preloader calling back before the last image was loaded.

--- Gruntfile.js
+++ Gruntfile.js
@@ -190,5 +190,5 @@ module.exports = function(grunt) {
       grunt.registerTask('default', ['sprites', 'concat', 'coffee', 'notify']);
    }
    grunt.registerTask('default', ['sprites', 'concat', 'coffee']);
-   grunt.registerTask('heroku:production', ['sprites','concat','coffee']);
+   grunt.registerTask('heroku:production', ['sprites','concat','coffee','uglify']);
 };
