5714962e39c634651e0fb5a529df316d0ee7de0b
Justin DuJardin <justin@dujardinconsulting.com>
2013-12-15 16:12:44 -0800

    Port over spriteRender.coffee, and move imageProcess.coffee back in to game/ after removing core's one dep on it.  World is all that's left in core.  Woo.

---
+++ src/core/spriteRender.ts
@@ -0,0 +1,76 @@
+/**
+ Copyright (C) 2013 by Justin DuJardin
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+///<reference path="../typedef/underscore/underscore.d.ts"/>
+///<reference path="../core/api.ts"/>
+///<reference path="../resources/image.ts"/>
+///<reference path="../core/resourceLoader.ts"/>
+module eburp {
+
+    export interface IWorld {
+        loader:ResourceLoader;
+    }
+    export interface IWorldObject {
+        world:IWorld;
+    }
+
+    export class SpriteRender implements IWorldObject {
+        canvas:HTMLCanvasElement = null;
+        context:CanvasRenderingContext2D = null;
+        world:IWorld = null;
+        constructor() {
+            this.canvas = document.createElement('canvas');
+            this.canvas.width = this.canvas.height = 16;
+
+            this.context = this.canvas.getContext('2d');
+            (<any>this.context).webkitImageSmoothingEnabled = false;
+            (<any>this.context).mozImageSmoothingEnabled = false;
+        }
+
+        getSpriteSheet(name:string,done:Function=()=>{}):ImageResource{
+            if(this.world){
+                return this.world.loader.load("/images/" + name + ".png",done);
+            }
+            return null;
+        }
+
+        getSingleSprite(spriteName:string,done:Function=(result:any)=>{}):ImageResource{
+            var coords:any = eburp.data.sprites[spriteName];
+            return this.getSpriteSheet(coords.source,(image:ImageResource)=>{
+                this.context.clearRect(0,0,this.canvas.width,this.canvas.height);
+                this.context.drawImage(image.data,coords.x,coords.y,this.canvas.width,this.canvas.height,0,0,this.canvas.width,this.canvas.height);
+                var src:string = this.canvas.toDataURL();
+                var result:HTMLImageElement = new Image();
+                result.src = src;
+                result.onload = function() {
+                    done(result);
+                };
+                result.onerror = function(err){
+                    done(err);
+                };
+            });
+        }
+
+        getSpriteCoords(name:string) {
+            var desc = eburp.data.sprites[name];
+            if(!desc){
+                throw new Error("Missing sprite data for: " + name);
+            }
+            return desc;
+        }
+    }
+
+}
--- web/directives/gameView.js
+++ web/directives/gameView.js
@@ -104,34 +104,35 @@ twoFiftySix.app.directive('gameView', function ($compile, game) {
 
             game.tileView = new eburp.TileMapView(element[0], game.loader);
             game.tileView.camera.extent.set(10, 10);
-            game.tileView.imageProcessor = new eburp.ImageProcessor(renderCanvas[0], game.tileView);
             game.tileView.tileMap = game.tileMap;
             game.scene.addView(game.tileView);
             game.tileView.trackObject(game.sprite);
 
+            function _done(image){
+               game.sprite.image = image;
+               dropContext.clearRect(0, 0, 96, 96);
+               dropContext.drawImage(image, 0, 0, 96, 96);
+            }
             var image = $(".user-sprite img")[0];
             var dropCanvas = $("canvas.image-drop")[0];
             var dropContext = dropCanvas.getContext("2d");
             var storedImage = $scope.getImageData();
             if (!image.src) {
                if (!storedImage) {
-                  var sprite = game.tileView.imageProcessor.isolateSprite("party.png");
-                  image.src = sprite.src;
+                  game.world.sprites.getSingleSprite("party.png",function(img){
+                     _done(img);
+                  });
                }
                else {
                   $scope.setImageData(storedImage);
                   image.src = storedImage;
                }
                image.onload = function () {
-                  game.sprite.image = image;
-                  dropContext.clearRect(0, 0, 96, 96);
-                  dropContext.drawImage(image, 0, 0, 96, 96);
+                  _done(image);
                };
             }
             else {
-               game.sprite.image = image;
-               dropContext.clearRect(0, 0, 96, 96);
-               dropContext.drawImage(image, 0, 0, 96, 96);
+               _done(image);
             }
             console.log("READY TO GO!");
          });
