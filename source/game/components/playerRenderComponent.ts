98076ca0cf07871296ab6d99d92b35fd5e153b6f
Justin DuJardin <justin@dujardinconsulting.com>
2014-04-06 14:53:28 -0700

    Move crazy animator into core/ and out of playerRenderComponent.

--- source/game/components/playerRenderComponent.ts
+++ source/game/components/playerRenderComponent.ts
@@ -18,55 +18,7 @@
 /// <reference path="../../tile/tileObject.ts" />
 
 module pow2 {
-   export class Animator{
-      interpFrame:number = 0;
-      animElapsed:number = 0;
-      animDuration:number = 0;
-      frames:number[] = [0];
 
-      sourceMeta:any = null;
-      sourceAnims:any = null;
-
-      setAnimationSource(spriteName:string){
-         console.log("Sprite is " + spriteName);
-         this.sourceMeta = pow2.getSpriteMeta(spriteName);
-         this.sourceAnims = this.sourceMeta.animations;
-         this.setAnimation('down');
-      }
-
-      setAnimation(name:string){
-         if(!this.sourceAnims){
-            throw new Error("Invalid source animations");
-         }
-         var data:any = this.sourceAnims[name];
-         if(!data){
-            throw new Error("Invalid animation name - " + name);
-         }
-         this.frames = data.frames;
-         this.animDuration = data.duration;
-      }
-
-      updateTime(elapsedMs:number){
-         this.animElapsed += elapsedMs;
-         var factor:number = this.animElapsed / this.animDuration;
-         var index = Math.round(this.interpolate(0,this.frames.length-1,factor));
-         this.interpFrame = this.frames[index];
-         if(this.animElapsed > this.animDuration){
-            this.animElapsed = this.animElapsed % this.animDuration;;
-         }
-      }
-
-      interpolate(from:number,to:number,factor:number):number {
-         factor = Math.min(Math.max(factor,0),1);
-         return (from * (1.0 - factor)) + (to * factor);
-      }
-
-      getFrame():number {
-         return this.interpFrame;
-      }
-   }
-   
-   
    export enum MoveFrames {
       LEFT = 10,
       RIGHT = 4,
@@ -88,38 +40,15 @@ module pow2 {
    }
    export class PlayerRenderComponent extends TickedComponent {
       host:TileObject;
-      _elapsed: number = 0;
-
-      private _lastFrame:number = 6;
-      private _renderFrame:number = 6;
       private _animator:Animator = new Animator();
       heading:Headings = Headings.WEST;
       animating:boolean = false;
-
-
       connectComponent():boolean{
          if(!super.connectComponent()){
             return false;
          }
          this._animator.setAnimationSource(this.host.icon);
          return true;
-      }
-
-      tick(elapsed:number){
-         this._elapsed += elapsed;
-         if (this._elapsed < this.tickRateMS) {
-            return;
-         }
-         // Don't subtract elapsed here, but take the modulus so that
-         // if for some reason we get a HUGE elapsed, it just does one
-         // tick and keeps the remainder toward the next.
-         this._elapsed = this._elapsed % this.tickRateMS;
-
-         // There are four states and two rows.  The second row is all alt states, so mod it out
-         // when a move ends.
-         this._lastFrame = this._renderFrame;// > 3 ? this._renderFrame - 4 : this._renderFrame;
-         super.tick(elapsed);
-         //console.log("Heading: " + Headings[this.heading] + " - " + this.animating);
       }
 
       setHeading(direction:Headings,animating:boolean){
