9c4db21e6106f64930c74598fa0aabbf0362ade6
Justin DuJardin <justin@dujardinconsulting.com>
2014-04-06 13:20:06 -0700

    Add scaffolded support for more generic player animations in playerRenderComponent.

--- source/game/components/playerRenderComponent.ts
+++ source/game/components/playerRenderComponent.ts
@@ -18,6 +18,55 @@
 /// <reference path="../../tile/tileObject.ts" />
 
 module pow2 {
+   export class Animator{
+      interpFrame:number = 0;
+      animElapsed:number = 0;
+      animDuration:number = 0;
+      frames:number[] = [0];
+
+      sourceMeta:any = null;
+      sourceAnims:any = null;
+
+      setAnimationSource(spriteName:string){
+         console.log("Sprite is " + spriteName);
+         this.sourceMeta = pow2.getSpriteMeta(spriteName);
+         this.sourceAnims = this.sourceMeta.animations;
+         this.setAnimation('down');
+      }
+
+      setAnimation(name:string){
+         if(!this.sourceAnims){
+            throw new Error("Invalid source animations");
+         }
+         var data:any = this.sourceAnims[name];
+         if(!data){
+            throw new Error("Invalid animation name - " + name);
+         }
+         this.frames = data.frames;
+         this.animDuration = data.duration;
+      }
+
+      updateTime(elapsedMs:number){
+         this.animElapsed += elapsedMs;
+         var factor:number = this.animElapsed / this.animDuration;
+         var index = Math.round(this.interpolate(0,this.frames.length-1,factor));
+         this.interpFrame = this.frames[index];
+         if(this.animElapsed > this.animDuration){
+            this.animElapsed = this.animElapsed % this.animDuration;;
+         }
+      }
+
+      interpolate(from:number,to:number,factor:number):number {
+         factor = Math.min(Math.max(factor,0),1);
+         return (from * (1.0 - factor)) + (to * factor);
+      }
+
+      getFrame():number {
+         return this.interpFrame;
+      }
+   }
+   
+   
    export enum MoveFrames {
       LEFT = 10,
       RIGHT = 4,
@@ -43,8 +92,19 @@ module pow2 {
 
       private _lastFrame:number = 6;
       private _renderFrame:number = 6;
+      private _animator:Animator = new Animator();
       heading:Headings = Headings.WEST;
       animating:boolean = false;
+
+
+      connectComponent():boolean{
+         if(!super.connectComponent()){
+            return false;
+         }
+         this._animator.setAnimationSource(this.host.icon);
+         return true;
+      }
+
       tick(elapsed:number){
          this._elapsed += elapsed;
          if (this._elapsed < this.tickRateMS) {
@@ -59,10 +119,25 @@ module pow2 {
          // when a move ends.
          this._lastFrame = this._renderFrame;// > 3 ? this._renderFrame - 4 : this._renderFrame;
          super.tick(elapsed);
+         //console.log("Heading: " + Headings[this.heading] + " - " + this.animating);
       }
 
       setHeading(direction:Headings,animating:boolean){
          this.heading = direction;
+         switch(this.heading){
+            case Headings.SOUTH:
+               this._animator.setAnimation('down');
+               break;
+            case Headings.NORTH:
+               this._animator.setAnimation('up');
+               break;
+            case Headings.EAST:
+               this._animator.setAnimation('right');
+               break;
+            case Headings.WEST:
+               this._animator.setAnimation('left');
+               break;
+         }
          this.animating = animating;
       }
 
@@ -72,17 +147,8 @@ module pow2 {
 
       interpolateTick(elapsed:number) {
          super.interpolateTick(elapsed);
-
-         // Choose frame for interpolated position
-         var factor = this._elapsed / this.tickRateMS;
-         var altFrame = !!((factor > 0.0 && factor < 0.5));
-         var frame = this._lastFrame;
-
-         frame = this.heading;
-         if(altFrame && this.animating){
-            frame += 1;
-         }
-         this.host.frame = this._renderFrame = frame;
+         this._animator.updateTime(elapsed);
+         this.host.frame = this._animator.getFrame();
       }
    }
 }
