2b370fbf61e12307619d98141701e7de20005616
Justin DuJardin <justin@dujardinconsulting.com>
2014-06-06 16:36:52 -0700

    docs: remove incorrect copy/paste docs
    - Combat view - renders a combat scene (todo: remove and use game view?)
    - Combat camera/encounter/render components.

--- source/game/components/damageComponent.ts
+++ source/game/components/damageComponent.ts
@@ -23,9 +23,6 @@ limitations under the License.
 
 module pow2 {
 
-   /**
-    * A component that defines the functionality of a map feature.
-    */
    export class DamageComponent extends SceneComponent {
       host:GameEntityObject;
       animation:AnimatedSpriteComponent;
